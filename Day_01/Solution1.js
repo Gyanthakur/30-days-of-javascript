0b6314e6484037d41dffa7260710e0b5bd5426bc
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-11-06 14:08:08 +0530

    challenge_#01

--- Day_01/Solution1.js
+++ Day_01/Solution1.js
@@ -12,27 +12,14 @@ Expected Output: 34223
 
 
 function reverseNumber(num) {
-    // Convert the number to a string.
     const strNum = num.toString();
-  
-    // Split the string into an array of characters.
     const arrNum = strNum.split('');
-  
-    // Reverse the array.
     arrNum.reverse();
-  
-    // Join the array back into a string.
     const reversedStrNum = arrNum.join('');
-  
-    // Convert the string back to a number.
     const reversedNum = Number(reversedStrNum);
-  
-    // Return the reversed number.
     return reversedNum;
   }
-  
-
 const x = 32243;
 const reversedNum = reverseNumber(x);
 
-console.log(reversedNum); // 34223
+console.log(reversedNum);
