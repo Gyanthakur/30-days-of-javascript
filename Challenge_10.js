38ad5fb6122c8da732ca894c17cd056447577a95
Gyanthakur <gps.96169@gmail.com>
2023-11-15 18:18:06 +0530

    challenge_#10
    Signed-off-by: Gyanthakur <gps.96169@gmail.com>

---
+++ Challenge_10.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+      for (let j = 0; j < array.length - i - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        }
+      }
+    }
+    return array;
+  }
+  const sampleArray = [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213];
+
+  const sortedArray = bubbleSort(sampleArray);
+  
+  console.log(sortedArray); 
+    
